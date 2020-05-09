9a7dc3bcb5bdbec4a8c9c79a156ea8e4346fe96a
Dimitar Terzov <dimiterat@gmail.com>
2020-05-09 12:05:59 +0300

    Swap places of commands 24 and 25.

--- src/tests/test_command_25.py
+++ src/tests/test_command_25.py
@@ -6,128 +6,160 @@ from command_25.validator_25 import command25
 
 
 CONTENT = [
-    u'<Turn startTime="0" endTime="1.351">\n',                         # 1
-    u'<Sync time="0"/>\n',                                             # 2
-    u'[music]\n',                                                      # 3
-    u'</Turn>\n',                                                      # 4
-    u'<Turn speaker="spk2" startTime="1.351" endTime="10.959">\n',     # 5
-    u'<Sync time="1.351"/>\n',                                         # 6
-    u'&lt;lang:Foreign&gt; (()) &lt;/lang:Foreign&gt;\n',              # 7
-    u'<Sync time="5.854"/>\n',                                         # 8
-    u'[music]\n',                                                      # 9
-    u'</Turn>\n',                                                      # 10
-    u'<Turn startTime="10.959" endTime="55.643" speaker="spk2">\n',    # 11
-    u'<Sync time="10.959"/>\n'                                                       # 12
-    u"Welcome to Insight, I'm Philippa Tolley. This week, the Pacific.\n",           # 13
-    u'<Sync time="22.275"/>\n',                                                      # 14
-    u'Its Prime Minister has been vocal on the world stage, trying to\n',            # 15
-    u'<Sync time="32.566"/>\n',                                                      # 16
-    u'But despite that gloomy outlook, the low line group of atolls and\n',          # 17
-    u'<Sync time="41.26"/>\n',                                                       # 18
-    u'Tuvalu has big plans for its future and its leaders that urging.\n',           # 19
-    u'<Sync time="49.963"/>\n',                                                      # 20
-    u'[music]\n',                                                                    # 21
-    u'<Sync time="51.747"/>\n',                                                      # 22
-    u'[noise]\n',                                                                    # 23
-    u'</Turn>\n',                                                                    # 24
-    u'<Turn speaker = " spk4 " startTime="55.643" endTime="112.245">\n',             # 25
-    u'<Sync time="55.643"/>\n',                                                      # 26
-    u"I bounce on the Funafuti lagoon. It seems a world away from Fulafani.\n",      # 27
-    u'<Sync time="67.998"/>\n',                                                      # 28
-    u"Behind me, I can just see a narrow strip of land peeping above the water.\n",  # 29
-    u'<Sync time="78.971"/>\n',                                                      # 30
-    u'[noise]\n',                                                           # 31
-    u'<Sync time="81.713"/> \n',                                            # 32
-    u'<Sync time="91.488"/>\n',                                             # 33
-    u'\n',                                                                 # 34
-    u'<Sync time="101.239"/>\n',                                            # 35
-    u'</Turn>\n',                                                           # 36
-    u'<Turn startTime="114.68" endTime="135.900">\n',                       # 37
-    u'<Sync time="114.68"/>\n',                                             # 38
-    u'</Turn>\n',                                                           # 39
-    u'<Turn speaker="spk4" startTime="138.482" endTime="152.316">\n',       # 40
-    u'<Sync time="138.482"/>\n',                                            # 41
-    u'\n',                                                                 # 42
-    u'</Turn>\n',                                                           # 43
-    u'<Turn speaker="spk5" startTime="152.316" endTime="156.423">\n',       # 44
-    u'<Sync time="152.316"/>\n',                                            # 45
-    u'&lt;lang:Foreign&gt; (()) &lt;/lang:Foreign&gt;\n',                   # 46
-    u'</Turn>\n',                                                           # 47
-    u'<Turn speaker="spk 5" startTime="160.404" endTime="161.589">\n',       # 48
-    u'<Sync time="160.404"/>\n',                                            # 49
-    u'&lt;lang:Foreign&gt; (()) &lt;/lang:Foreign&gt;\n',                   # 50
-    u'</Turn>\n',                                                           # 51
-    u'<Turn speaker="spk5" startTime="197.187" endTime="210.034">\n',       # 52
-    u'<Sync time="197.187"/>\n',                                            # 53
-    u"Mr. ((Lucia)) has to go further out into the lagoon to get fish.\n",  # 54
-    u'<Sync time="208.49"/>\n',                                             # 55
-    u'</Turn>\n',                                                           # 56
-    u'<Turn startTime="210.034" endTime="211.351">\n',                      # 57
-    u'<Sync time="210.034"/>\n',                                            # 58
-    u'\n',                                                                 # 59
-    u'</Turn>\n',                                                           # 60
-    u'<Turn speaker="spk64" startTime="3391.391" endTime="3391.334">\n',    # 61
-    u'<Sync time="3391.391"/>\n',                                           # 62
-    u'김아영씨 저희가 심폐소생 해드렸습니다.\n',                               # 63
-    u'</Turn>\n',                                                           # 64
-    u'<Turn speaker="spk4" startTime="197.187" endTime="210.034">\n',       # 65
-    u'<Sync time="196.187"/>\n',                                            # 66
-    u"Mr. ((Lucia)) has to go further out into the lagoon to get fish.\n",  # 67
-    u'</Turn>\n',                                                           # 68
-    u'<Turn speaker="spk5" startTime =" 197.187 " endTime= " 210.034 ">\n', # 69
-    u'<Sync time=" 196.187"/>\n',                                           # 70
-    u"Mr. ((Lucia)) has to go further out into the lagoon to get fish.\n",  # 71
-    u'<Sync time= "197.187"/>\n',                                           # 72
-    u"Mr. ((Lucia)) has to go further out into the lagoon to get fish.\n",  # 73
-    u'<Sync time ="197.187"/>\n',                                           # 74
-    u"Mr. ((Lucia)) has to go further out into the lagoon to get fish.\n",  # 75
-    u'</Turn>\n',                                                           # 76
-    u'<Turn speaker ="spk6" startTime=" 197.187" endTime="210.034 ">\n',    # 77
-    u'<Sync time="197.187"/>\n',                                            # 78
-    u"Mr. ((Lucia)) has to go further out into the lagoon to get fish.\n",  # 79
-    u'<Sync time="197.187 "/>\n',                                           # 80
-    u"Mr. ((Lucia)) has to go further out into the lagoon to get fish.\n",  # 81
-    u'</Turn>    \n',                                                       # 82
-    u'<Turn startTime="454.698" endTime="460.184">\n',                      # 83
-    u'<Sync time=" 454.698"/>\n',                                           # 84
-    u'[no-speech]\n',                                                       # 85
-    u'<Sync time="459.292"/>\n',    # 86
-    u'\n',                                            # 87
-    u'Oh, man. One of our greatest white allies and one of the [laugh]\n',    # 88
-    u'<Sync time="460.100"/>\n',    # 89
-    u'Oh, man. One of our greatest white allies and one of the [laugh]\n',    # 90
-    u'\n',                                                                                      # 91
-    u'<Sync time="460.424"/>\n',                                            # 92
-    u'[no-speech]\n',                                                       # 93
-    u'<Sync time="462.832"/>\n',                                            # 94
-    u'Sync Ike Rann ja tema uus album &lt;lang:English&gt; Synchronize &lt;/lang:English&gt; Me kuulame\n',  # 95
-    u'</Turn>\n',                                                           # 96
+    u'<Turn startTime="0" endTime ="14.781" speaker="spk1">\n',    # 1
+    u'<Syncs time="0"/>\n',                                         # 2
+    u'[overlap]\n'                                                 # 3
+    u'</Turn>\n',                                                  # 4
+    u'<Turn speaker="spk2" startTime="14.781" endTime=" 21.081">\n',  # 5
+    u'<Sync time=" 15.781"/>\n',                                      # 6
+    u'[noise] [music] 물러나시오! 임금님 행차하신다.\n',                # 7
+    u'</Turn>\n',                                                     # 8
+    u'<Turn speaker="spk3" startTime="21.081 " endTime="32.282">\n',  # 9
+    u'<Sync time= "28.081"/>\n',                                      # 10
+    u'[laugh] [applause] 보이느냐? 나의 이 멋진 옷이! [laugh]\n',       # 11
+    u'</Turn>\n',                                                     # 12
+    u'<Turn speaker="spk4" startTime="32.282" endTime="39.087 ">\n',  # 13
+    u'<Sync time=" 32.282"/>\n',                                      # 14
+    u'오, 그렇다면 나는 랜덤 캐릭터. [noise]\n',                        # 15
+    u'</Turn>\n',                                                     # 16
+    u'<Turn speaker="spk5 " startTime="39.087" endTime="43.950">\n',  # 17
+    u'<Sync time="39.087 "/>\n',                                      # 18
+    u'아, 이 아저씨가 여기서 또 옷 벗고 있네? 따라와요.\n',              # 19
+    u'</Turn>\n',                                                     # 20
+    u'<Turn speaker=" spk1" startTime="43.950" endTime="58.756">\n',  # 21
+    u'<Sync time= "43.950"/>\n',                                      # 22
+    u'[overlap]\n',                                                   # 23
+    u'</Turn>\n',                                                     # 24
+    u'<Turn speaker= "spk6" startTime="58.756" endTime="68.486">\n',  # 25
+    u'<Sync time ="58.756"/>\n',                                              # 26
+    u'[music] 아하. [noise] 어, 우와. 내 머리는 길지만 니 수명을 짧을 거야.\n',  # 27
+    u'</Turn>\n',                                                             # 28
+    u'<Turn speaker="spk2" startTime="956.003" endTime="957.894">\n',         # 29
+    u'<Sync time="59.003"/>\n',                                               # 30
+    u'Thank you candidate Graham. [overlap] Thanks.\n',                       # 31
+    u'</Turn>\n',                                                             # 32
+    u'<Turn speaker ="spk4" startTime="68.486" endTime="73.614">\n',          # 33
+    u'<Sync tme="68.486"/>\n',                                                # 34
+    u'[laugh] 그래. 그럼 나는 영화 어 아저씨의 멋있는 원빈! [noise]\n',          # 35
+    u'</Turn>\n',                                                             # 36
+    u'<Turn startTime ="3170.236" endTime="3171.857">\n',                     # 37
+    u'<Sync time="3170.236"/>\n',                                             # 38
+    u'[noise] &lt;lang:Foreign&gt;ČSSDspacing 5ČSSD &lt;/lang:Foreign&gt;\n', # 39
+    u'</Turn>\n',                                                             # 40
+    u'<Turn startTime= "3170.236" endTime="3171.857">\n',                     # 41
+    u'<Turn startTime=" 3170.236" endTime="3171.857">\n',                     # 42
+    u'<Turn startTime="3170.236 " endTime="3171.857">\n',                     # 43
+    u'<Turn startTime ="3170.236" endTime="3171.857">\n',                     # 44
+    u'<Turn startTime="3170.236" endTime= "3171.857">\n',                     # 45
+    u'<Turn startTime="3170.236" endTime="3171.857" speaker="spk2 ">\n',      # 46
+    u'<Turn startTime="3170.236" endime="3171.857" speaker="spk2">\n',        # 47
+    u'<Turn speaker="spk2" starterTime="3170.236" endTime="3171.857">\n',     # 48
+    u'<Turn speakers="spk2" startTime="3170.236" endTime="3171.857">\n',      # 49
+    u'<Turns speaker="spk2" startTime="3170.236" endTime="3171.857">\n',      # 50
+    u'<Turn startTime="3170.236" endTime="3171.857" speaker="spker2">\n',     # 51
+    u'<Speaker id="spk1" name="Paul Donato" check="no" type="male" dialect="native" accent="" scope="global"/>\n',   # 52
+    u'<Speaker id="spk2" name="Henry Milorin" check="no" type="male" dialect="native" accent="" scope="global"/>\n', # 53
+    u'<Speaker id="spk12" name="Kathleen Kreatz" \n',                                                                # 54
+    u'check="no" type="female" dialect="native" accent="" scope="global"/>\n',                                       # 55
+    u'<Speaker id="spk3" name="Mike Ruggiero" check="no" type="female" dialect="native" accent="" scope="global"/>\n'# 56
+
+    u'<Turn speaker="spk2" startTime="3170.236" endTime="3171.857">\n',    # 57
+    u'<Sync time="58.756"/>\n',                                 # 58
+    u'\n',                                                                        # 59
+    u'[music] 아하. [noise] 어, 우와. 수명을 짧을 거야.\n',    # 60
+    u'</Turn>\n',                                                           # 61
+    u'<Turn speaker="spk2" startTime="3170.236" endTime="3171.857">\n',    # 62
+    u'<Sync time="58.756"/>\n',                                 # 63
+    u'[music] 아하. [noise] 어, 우와.\n',                             # 64
+    u'\n',                                                                         # 65
+    u'<Sync time="58.756"/>\n',                                  # 66
+    u'[music] 아하. [noise] 어, 우와.\n',                             # 67
+    u'<Sync time="0"/>\n',                                           # 68
+    u'\n',                                                                         # 69
+    u'<Sync time="0"/>\n',                                           # 70
+    u'\n',                                                                         # 71
+    u'</Turn>\n'                                                             # 72
 ]
-EXCLUDE = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15,
-    16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-    38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49,
-    50, 51, 53, 54, 55, 56, 57, 58, 59, 60, 61,
-    62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,
-    73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
-    84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94,
-    95, 96
-]
-CATCH = [11, 48, 52]
 
 
-def test_command_25(tmpdir):
+def test_command25(tmpdir):
     file_ = temporary_file(tmpdir, CONTENT)
     found = command25(file_)
 
     for key in sorted(found.keys()):
         print(key, found[key])
 
-    for row in EXCLUDE:
-        assert row not in found
-
-    for row in CATCH:
-        assert row in found
+    assert "Unexpected white space in Turn tag" in found[1]
+    assert 'Tag syntax error' in found[2]
+    assert not 3 in found
+    assert not 4 in found
+    assert "Unexpected white space in Turn tag" in found[5]
+    assert "Unexpected white space in Sync tag" in found[6]
+    assert not 7 in found
+    assert not 8 in found
+    assert "Unexpected white space in Turn tag" in found[9]
+    assert "Unexpected white space in Sync tag" in found[10]
+    assert not 11 in found
+    assert not 12 in found
+    assert "Unexpected white space in Turn tag" in found[13]
+    assert "Unexpected white space in Sync tag" in found[14]
+    assert not 15 in found
+    assert not 16 in found
+    assert "Unexpected white space in Turn tag" in found[17]
+    assert "Unexpected white space in Sync tag" in found[18]
+    assert not 19 in found
+    assert not 20 in found
+    assert "Unexpected white space in Turn tag" in found[21]
+    assert "Unexpected white space in Sync tag" in found[22]
+    assert not 23 in found
+    assert not 24 in found
+    assert "Unexpected white space in Turn tag" in found[25]
+    assert "Unexpected white space in Sync tag" in found[26]
+    assert not 27 in found
+    assert not 28 in found
+    assert not 29 in found
+    assert not 30 in found
+    assert not 31 in found
+    assert not 32 in found
+    assert "Unexpected white space in Turn tag" in found[33]
+    assert "Tag syntax error" in found[34]
+    assert not 35 in found
+    assert not 36 in found
+    assert "Unexpected white space in Turn tag" in found[37]
+    assert 38 not in found
+    assert not 39 in found
+    assert not 40 in found
+    assert "Unexpected white space in Turn tag" in found[41]
+    assert "Unexpected white space in Turn tag" in found[42]
+    assert "Unexpected white space in Turn tag" in found[43]
+    assert "Unexpected white space in Turn tag" in found[44]
+    assert "Unexpected white space in Turn tag" in found[45]
+    assert "Unexpected white space in Turn tag" in found[46]
+    assert "Tag syntax error" in found[47]
+    assert "Tag syntax error" in found[48]
+    #assert 49 in found
+    assert "Tag syntax error" in found[50]
+    #assert 51 in found
+    assert 52 not in found
+    assert 53 not in found
+    assert "Tag syntax error" in found[54]
+    assert 55 not in found
+    assert 56 not in found
+    assert "Empty row in Sync tag" in found[59]
+    assert "Empty row in Sync tag" in found[65]
+    assert 57 not in found
+    assert 58 not in found
+    assert 60 not in found
+    assert 61 not in found
+    assert 62 not in found
+    assert 63 not in found
+    assert 64 not in found
+    assert 66 not in found
+    assert 67 not in found
+    assert 68 not in found
+    assert 69 not in found
+    assert 70 not in found
+    assert 71 not in found
+    assert 72 not in found
 
     #assert 0
